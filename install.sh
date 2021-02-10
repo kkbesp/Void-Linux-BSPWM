4e0e08c82e55e1654f09495e918112b3a5731783
kkbesp <kkbesp@gmail.com>
2021-02-10 13:22:58 +0500

    theme add

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo xbps-install -Suy &&
-sudo xbps-install -Sy xorg base-devel git curl wget libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel bspwm sxhkd dunst flameshot htop neovim polybar python jq font-weather-icons font-awesome5 noto-fonts-cjk noto-fonts-emoji picom numlockx hsetroot firefox chrony lxappearance rofi &&
+sudo xbps-install -Sy xorg base-devel git curl wget libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel bspwm sxhkd dunst flameshot htop neovim polybar python jq font-weather-icons font-awesome5 noto-fonts-cjk noto-fonts-emoji picom numlockx hsetroot firefox chrony lxappearance rofi gtk-engine-murrine gtk2-engines &&
 sudo ln -s /etc/sv/chronyd /var/service/ &&
 sudo cp .alias_zsh ~/ &&
 sudo cp .zshrc ~/ &&
@@ -24,7 +24,10 @@ cd st &&
 make &&
 sudo make install &&
 cd .. &&
-sudo chmod +x ~/.config/polybar/scripts/*
+sudo chmod +x ~/.config/polybar/scripts/* &&
+cd ctlos-qogir &&
+sudo bash install.sh &&
+cd .. &&
 sudo xbps-remove -Oo &&
 sudo cd .. &&
 sudo rm -rf dots &&
