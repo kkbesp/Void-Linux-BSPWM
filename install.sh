b74c4666fd523b0b3837eacad6cef645360a0f1f
kkbesp <52947109+kkbesp@users.noreply.github.com>
2026-01-15 20:24:59 +0500

    Update install script for modern setup

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo xbps-install -Suy &&
+set -euo pipefail
+
+sudo xbps-install -Syu &&
 sudo xbps-install -Sy xorg base-devel git curl wget libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel bspwm sxhkd dunst flameshot htop neovim polybar python jq font-weather-icons font-awesome5 noto-fonts-cjk noto-fonts-emoji picom numlockx hsetroot firefox chrony lxappearance rofi gtk-engine-murrine gtk2-engines font-iosevka void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree xtools zsh &&
 sudo ln -s /etc/sv/chronyd /var/service/ &&
 cp .zshrc ~ &&
@@ -9,9 +11,9 @@ cp .p10k.zsh ~ &&
 cp -r .config/ ~ &&
 cp -r .icons/ ~ &&
 cp -r .fonts/ ~ &&
-mkdir ~/.zinit &&
-git clone https://github.com/zdharma/zinit.git ~/.zinit/bin &&
-sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/ &&
+mkdir -p ~/.zinit &&
+git clone https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin &&
+sudo ln -sf /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/ &&
 sudo xbps-reconfigure -f fontconfig &&
 sudo fc-cache -f -v &&
 cd ~/Void-Linux-BSPWM/terminal &&
